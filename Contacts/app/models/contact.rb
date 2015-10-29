b0c09ca3dfc191ba1b535231e98bcfafda5cc9af
Arian Treffer <derari@web.de>
2015-10-29 13:15:36 +0100

    Implemented Contact#age

--- Contacts/app/models/contact.rb
+++ Contacts/app/models/contact.rb
@@ -1,3 +1,12 @@
 class Contact < ActiveRecord::Base
-
+  
+	def age
+		unless date_of_birth.is_a?(Date)
+			return nil
+		end
+		age = Date.today.year - date_of_birth.year
+		age -= 1 if Date.today < date_of_birth + age.years #for days before birthday
+		age = 0 if age < 0
+		return age
+	end
 end
